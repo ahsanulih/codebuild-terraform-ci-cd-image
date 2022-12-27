518d86b39f6c9ffb8009c84d2baff62988ed6a3c
AHSANUL INSAN HAMID <ahsanul.hamid@traveloka.com>
2022-12-27 11:20:17 +0700

    revert integration with terraform plan script

--- scripts/ci-do-terraform-plan.sh
+++ scripts/ci-do-terraform-plan.sh
@@ -18,6 +18,7 @@ if [ "$TF_WORKING_DIR" != "" ]; then
     cd $TF_WORKING_DIR
     terraform init -no-color 2> /tmp/errMsg.log
     terraform plan -out=terraform.tfplan -no-color 2> /tmp/errMsg.log
-    /bin/bash /usr/local/bin/ci-check-infra-cost.sh
+    cat aws-resource-charges-estimations
     cd -
 fi
+
