c6452f37316758857af2238a5dadeac1025d4010
AHSANUL INSAN HAMID <ahsanul.hamid@traveloka.com>
2022-12-30 08:43:23 +0700

    separate infra cost script

--- scripts/ci-check-infra-cost.sh
+++ scripts/ci-check-infra-cost.sh
@@ -2,11 +2,16 @@
 result=${PWD##*/}   # to assign current directory name to a variable
 result=${result:-/} # to correct for the case where PWD=/
 
-FILE="$result"-plan.json
-if [ ! -s "$FILE" ]; then
-    terraform plan -out "$result"-plan
-    terraform show -json "$result"-plan >"$result"-plan.json
+echo "Working dir : $TF_WORKING_DIR"
+if [ "$TF_WORKING_DIR" != "" ]; then
+    cd $TF_WORKING_DIR
+    FILE="$result"-plan.json
+    if [ ! -s "$FILE" ]; then
+        terraform plan -out "$result"-plan
+        terraform show -json "$result"-plan >"$result"-plan.json
+    fi
 fi
+
 
 python3 /usr/local/bin/infra-cost-estimator/scripts/main.py "$result"-plan.json >>aws-resource-charges-estimations
 
