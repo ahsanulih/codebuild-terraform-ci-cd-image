a5fd537d9175363452424da5e8fd0623471add6a
AHSANUL INSAN HAMID <ahsanul.hamid@traveloka.com>
2023-04-02 19:13:44 +0700

    fix script path

--- scripts/ci-do-static-app-security-testing.sh
+++ scripts/ci-do-static-app-security-testing.sh
@@ -8,6 +8,17 @@ checkov --version
 echo "#EXECUTING CHECKOV SUMMARIZED TEST"
 python3 /usr/local/bin/checkov-scripts/failed_summarizer.py -d $(pwd) >>sast-result-summary
 
+if [ $? -eq 0 ]; then
+    echo -e "\nSuccessfully executed summarized static app security test using Checkov"
+else
+    echo -e "\nThere were some failed checks during summarized static app security test using Checkov" >&2
+fi
+
+echo "ls -la"
+ls -la
+echo "cat sast-result-summary"
+cat sast-result-summary
+
 echo "Working dir : $TF_WORKING_DIR"
 if [ "$TF_WORKING_DIR" != "" ]; then
     cd $TF_WORKING_DIR
@@ -22,9 +33,9 @@ echo "#EXECUTING CHECKOV DETAILED TEST"
 checkov -f "$result"-plan.json >>sast-result-detail
 
 if [ $? -eq 0 ]; then
-    echo -e "\nSuccessfully executed static app security test using Checkov"
+    echo -e "\nSuccessfully executed detailed static app security test using Checkov"
     exit 0
 else
-    echo -e "\nThere were some failed checks during static app security test using Checkov" >&2
+    echo -e "\nThere were some failed checks during detailed static app security test using Checkov" >&2
     exit 0
 fi
